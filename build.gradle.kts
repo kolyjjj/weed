1def19e2f78ff5770e0a1ce2ebf43d1c495397f1
koly <kolyjjj@foxmail.com>
2016-10-20 14:12:28 +0800

    add common dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,13 @@
 subprojects{
     apply<JavaPlugin>()
+
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        compile("com.google.guava:guava:20.0-rc1")
+        compile("joda-time:joda-time:2.9.4")
+        compile("org.springframework.boot:spring-boot-starter-data-jpa:1.4.1.RELEASE")
+    }
 }
