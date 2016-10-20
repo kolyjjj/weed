72fc3711cd4038af2e7e6e0a03793ba7911ac11e
koly <kolyjjj@foxmail.com>
2016-10-20 16:53:06 +0800

    rename user application configuration

--- user/src/main/java/li/koly/user/configuration/UserConfiguration.java
+++ user/src/main/java/li/koly/user/configuration/UserConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ApplicationConfiguration {
+public class UserConfiguration {
     @Bean
     public UserService userService(UserRepository userRepository) {
         return new UserService(userRepository);
