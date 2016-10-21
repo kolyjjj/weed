ba360bc0f5f7dae2a088530b0302997256f0cf0c
koly <kolyjjj@foxmail.com>
2016-10-21 17:50:04 +0800

    to serve image from an folder

--- user/src/main/java/li/koly/user/configuration/UserConfiguration.java
+++ user/src/main/java/li/koly/user/configuration/UserConfiguration.java
@@ -4,11 +4,18 @@ import li.koly.user.repository.UserRepository;
 import li.koly.user.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class UserConfiguration {
+public class UserConfiguration extends WebMvcConfigurerAdapter {
     @Bean
     public UserService userService(UserRepository userRepository) {
         return new UserService(userRepository);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images/**").addResourceLocations("file:/Users/kwli/program/gradle/weed/images/");
     }
 }
