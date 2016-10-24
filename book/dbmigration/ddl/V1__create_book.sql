8558dc1d6458c4bac0ecc9d33e177378fca0548b
koly <kolyjjj@foxmail.com>
2016-10-24 15:18:16 +0800

    add flyway scripts

---
+++ book/dbmigration/ddl/V1__create_book.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS book;
+CREATE TABLE book (
+    isbn INT NOT NULL UNIQUE,
+    title VARCHAR(100) NOT NULL UNIQUE,
+    subtitle VARCHAR(100),
+    author VARCHAR(50),
+    PRIMARY KEY(isbn)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
